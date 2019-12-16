abfe84c57ca1d7b4e45fc14ecca93c45f6118a42
Luciano Rolando Lorenti <lucianolorenti@gmail.com>
2019-12-16 18:54:49 +0100

    Add the posibility to join the sentence after tokenizing

--- text_analysis/text_processing.py
+++ text_analysis/text_processing.py
@@ -1,5 +1,5 @@
 from stop_words import get_stop_words
-from nltk.tokenize import RegexpTokenizer,  word_tokenize
+from nltk.tokenize import RegexpTokenizer, word_tokenize
 from nltk.corpus import stopwords
 import operator
 import re
@@ -13,33 +13,45 @@ from many_stop_words import get_stop_words
 def lowercase(d):
     return d.lower()
 
+
 def apostrophes(s):
     return s.replace("'", "")
+
 
 def letter_digit_underscore(d):
     pat = r'^\W+|\W+$'
     return re.sub(pat, ' ', d)
 
+
 def remove_space_newline_tab(d):
-    return re.sub( r'\s', ' ', d)
+    return re.sub(r'\s', ' ', d)
+
 
 def remove_punctuation(d):
     return re.sub(r'[^a-zA-Z0-9]', ' ', d)
-    
+
 
 def remove_digits(d):
     return re.sub('\d+', '', d)
 
-DEFAULT_PROCESS = [lowercase, apostrophes, letter_digit_underscore, remove_space_newline_tab, remove_punctuation, remove_digits, word_tokenize]
 
-def clean_content(data, process, stop_words):
-    words = []
+DEFAULT_PROCESS = [
+    lowercase, apostrophes, letter_digit_underscore, remove_space_newline_tab,
+    remove_punctuation, remove_digits
+]
+
+
+def clean_content(data, process, stop_words, join=False):
+    documents = []
     for d in data:
         w = d
         for process_fun in process:
             w = process_fun(w)
-        words.append(w)
-    return [word for word in words if word not in stop_words]
+        sentence = [word for word in word_tokenize(w) if word not in stop_words]
+        if join:
+            sentence = ' '.join(sentence)
+        documents.append(sentence)
+    return documents
 
 
 def cleaning_tokenize(text, ln):
@@ -74,7 +86,7 @@ def get_stopwords(lang):
         nltk_words = set(stopwords.words('spanish'))  # About 150 stopwords
     if lang == 'pt':
         nltk_words = set(stopwords.words('portuguese'))  # About 150 stopwords
-    m_stop_words = set (get_stop_words(lang))
+    m_stop_words = set(get_stop_words(lang))
     stop_words.update(nltk_words)
     stop_words.update(m_stop_words)
     return stop_words
@@ -83,8 +95,6 @@ def get_stopwords(lang):
 def add_other_stopwords(stop_words, other_stop_words):
     stop_words.update(other_stop_words)
     return stop_words
-
-
 
 
 def get_topics_distribution(categories, min_occurences=12):
@@ -111,7 +121,10 @@ def get_topics_distribution(categories, min_occurences=12):
         for k in remove:
             del probabilities[category][k]
         cant_words = sum(probabilities[category].values())
-        probabilities[category] = {k: v / cant_words for k, v in probabilities[category].items()}
+        probabilities[category] = {
+            k: v / cant_words
+            for k, v in probabilities[category].items()
+        }
 
     return probabilities
 
@@ -124,13 +137,15 @@ def get_p_b_d(document, wa, wb):
         n_biterms = n_biterms + 1
         if (w1 == wa) and (w2 == wb):
             biterm = biterm + 1
-    return biterm/n_biterms
+    return biterm / n_biterms
 
 
 def topic_assignment(question, topic_distribution):
     total_words = sum([len(topic) for topic in topic_distribution.values()])
-    p_z = {topic_name: len(topic) / total_words
-           for topic_name, topic in topic_distribution.items()}
+    p_z = {
+        topic_name: len(topic) / total_words
+        for topic_name, topic in topic_distribution.items()
+    }
     p_z_d = dict()
     for j in topic_distribution.keys():
         p_z_d[j] = 0.0
@@ -140,16 +155,18 @@ def topic_assignment(question, topic_distribution):
         for j in topic_distribution.keys():
             t_d_w1 = 0.00000001
             t_d_w2 = 0.00000001
-            if w1 in topic_distribution[j] and topic_distribution[j][w1] > 0.00000001:
-                    t_d_w1 = topic_distribution[j][w1]
-            if w2 in topic_distribution[j] and topic_distribution[j][w2] > 0.00000001:
-                    t_d_w2 = topic_distribution[j][w2]
+            if w1 in topic_distribution[
+                    j] and topic_distribution[j][w1] > 0.00000001:
+                t_d_w1 = topic_distribution[j][w1]
+            if w2 in topic_distribution[
+                    j] and topic_distribution[j][w2] > 0.00000001:
+                t_d_w2 = topic_distribution[j][w2]
             temp[j] = p_z[j] * t_d_w1 * t_d_w2
         deno = sum(temp.values()) + 0.00000001
-        p_z_b = {j: t / deno for j,t in temp.items()}
+        p_z_b = {j: t / deno for j, t in temp.items()}
         for j in topic_distribution.keys():
             p_z_d[j] = p_z_d[j] + p_z_b[j] * p_b_d
-    if all(value <= 0.00000001*0.00000001 for value in p_z_d.values()):
+    if all(value <= 0.00000001 * 0.00000001 for value in p_z_d.values()):
         prob = 'general'
     else:
         prob = max(p_z_d.items(), key=operator.itemgetter(1))[0]
@@ -160,8 +177,9 @@ if __name__ == '__main__':
     topics_dist = get_topics_distribution()
     #max_word = sorted(topics_dist['basketball'].items(), key=operator.itemgetter(1))[::-1][1:10]
     #print(max_word)
-    print("prob de kung en artes marciales " + str(topics_dist['martial arts']['kung']))
-    print (topics_dist['tennis'])
+    print("prob de kung en artes marciales " +
+          str(topics_dist['martial arts']['kung']))
+    print(topics_dist['tennis'])
     #question = 'How many points does a volleyball set have?'
     # me la clasifica de beach volleyball y no volleyball
     question = 'Where is Kung Fu from? USA Japan Antarctica China USA Japan Antarctica China'
